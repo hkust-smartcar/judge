19449f74ed0fd7168433cc5037be17431b2a3b12
mcreng <tsehonam@gmail.com>
2018-12-25 14:20:05 +0800

    feat: firejail memory limit and timeout

--- src/server/routes/submit/queue.js
+++ src/server/routes/submit/queue.js
@@ -8,31 +8,50 @@ const jobQueue = new Queue("job", {
 
 jobQueue.on("ready", () => {
   jobQueue.process(async job => {
-    console.log("processing job " + job.id);
-    console.log(job.data.files);
-
     let file = job.data.files[0];
-    console.log(file);
+    console.log(`Processing job ${job.id} with name ${file.originalname}`);
     let filetype = file.originalname.split(".").pop();
 
     switch (filetype) {
       case "py":
         console.log("Handling a Python file.");
+
+        /**
+         * firejail: sandbox
+         * --overlay-tmpfs: Overlay a temporary filesystem which does not affect the real one
+         * --quiet: No messages from firejail itself
+         * --noprofile: Do not look for any predesigned firejail profiles
+         * --net=none: No internet access
+         * --rlimit-as=: RAM limit in Bytes
+         * --timeout=: Time out. For python, we need to add 3s for startup time.
+         * python3: Run python3
+         */
         let output = await exec(
-          `firejail --overlay-tmpfs --quiet --noprofile --net=none python ${
+          `firejail --overlay-tmpfs --quiet --noprofile --net=none --rlimit-as=134217728 --timeout=00:00:04 python3 ${
             file.path
           }`
-        );
+        ).catch(err => {
+          // If no stderr => time limit exceeded
+          if (err.stderr === "") {
+            throw new Error("Time Limit Exceeded");
+          } else {
+            // check error message from python output
+            let errMsg = err.stderr.split("\n")[
+              err.stderr.split("\n").length - 2
+            ];
+            console.log(errMsg);
+            // if message contains MemoryError, throw Memory Error
+            if (errMsg.includes("MemoryError")) throw new Error("Memory Error");
+            // otherwise they are runtime errors
+            else throw new Error("Runtime Error");
+          }
+        });
 
         console.log(`stdout: ${output.stdout}`);
-        console.log(`stderr: ${output.stderr}`);
-
-        if (output.stderr) throw new Error("Runtime error");
         return output.stdout;
 
       default:
-        console.log("invalid file type " + job.id);
-        return new Error("Invalid file type");
+        return new Error("Invalid File Type");
     }
   });
 });

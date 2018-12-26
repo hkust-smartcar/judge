2b7d72ee7cc5e24d3b68ed1d1a127e4b4a098311
mcreng <tsehonam@gmail.com>
2018-12-26 18:06:47 +0800

    feat: pointwise scoring scheme

--- src/server/routes/submit/queue.js
+++ src/server/routes/submit/queue.js
@@ -17,6 +17,7 @@ const jobQueue = new Queue("job", {
 
 const execPyQueue = require("./execPy");
 const execCppQueue = require("./execCpp");
+const evaluate = require("./grade");
 
 jobQueue.on("ready", () => {
   jobQueue.process(async job => {
@@ -75,7 +76,11 @@ jobQueue.on("ready", () => {
 
     for (subtask of subtasks) {
       for (dataset of subtask["dataset"]) {
-        console.log(`Handling input ${dataset["input"]}`);
+        let output = dataset["output"];
+        let maxScore = dataset["points"];
+        console.log(
+          `Handling input ${dataset["input"]} with output ${dataset["output"]}`
+        );
         // Create job
         const execJob = queue.createJob({
           cmd,
@@ -94,7 +99,7 @@ jobQueue.on("ready", () => {
               question_id: parseInt(qid),
               subtask_id: subtask["id"],
               status: "Completed",
-              result
+              score: evaluate(result, output, questions[qid]["type"], maxScore)
             });
           })
 

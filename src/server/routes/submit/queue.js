f649a31482328fc25536de53b6898543422b007b
mcreng <tsehonam@gmail.com>
2018-12-26 16:43:36 +0800

    feat: cpp exec queue with I/O

--- src/server/routes/submit/queue.js
+++ src/server/routes/submit/queue.js
@@ -16,6 +16,7 @@ const jobQueue = new Queue("job", {
 });
 
 const execPyQueue = require("./execPy");
+const execCppQueue = require("./execCpp");
 
 jobQueue.on("ready", () => {
   jobQueue.process(async job => {
@@ -27,6 +28,9 @@ jobQueue.on("ready", () => {
 
     console.log(`Processing job ${job.id} with name ${file.originalname}`);
     let filetype = file.originalname.split(".").pop();
+
+    let queue = undefined;
+    let cmd = undefined;
 
     switch (filetype) {
       case "py": {
@@ -41,43 +45,11 @@ jobQueue.on("ready", () => {
          * --timeout=: Time out. For python, we need to add 3s for startup time.
          * python3: Run python3
          */
-        let cmd = `firejail --overlay-tmpfs --quiet --noprofile --net=none --rlimit-as=134217728 --timeout=00:00:04 python3 ${
+        cmd = `firejail --overlay-tmpfs --quiet --noprofile --net=none --rlimit-as=134217728 --timeout=00:00:04 python3 ${
           file.path
         }`;
-        for (subtask of subtasks) {
-          for (dataset of subtask["dataset"]) {
-            console.log(`Handling input ${dataset["input"]}`);
-            // Create job
-            const job = execPyQueue.createJob({ cmd, input: dataset["input"] });
-
-            // On job successful, emit socket
-            job
-
-              .on("succeeded", function(result) {
-                console.log("completed job " + job.id + " result ", result);
-                io.to(user).emit("alert", {
-                  type: "result",
-                  job_id: job.id,
-                  status: "Completed",
-                  result
-                });
-              })
-
-              // On job failure, emit socket
-              .on("failed", err => {
-                console.log(`job failed with error ${err.message}.`);
-                io.to(user).emit("alert", {
-                  type: "result",
-                  job_id: job.id,
-                  status: "Completed",
-                  error: err.message
-                });
-              })
-
-              .save();
-          }
-        }
-        return;
+        queue = execPyQueue;
+        break;
       }
 
       case "cpp": {
@@ -91,31 +63,48 @@ jobQueue.on("ready", () => {
         await exec(`rm ${file.path}.cpp`);
 
         console.log("C++ compiled.");
-
-        let output = await exec(
-          `firejail --overlay-tmpfs --quiet --noprofile --net=none --rlimit-as=134217728 --timeout=00:00:04 ${
-            file.path
-          }`
-        ).catch(err => {
-          console.log(err);
-          if (err.stderr.includes("std::bad_alloc")) {
-            // C++ returns std::bad_alloc if new operators can no longer allocate memory
-            // malloc should be fine because if no memory can be allocated, malloc returns NULL and
-            // does not throw exceptions.
-            throw new MemoryError();
-          } else if (err.code == 1) {
-            // code 1 means firejail terminated the procedure => time limit exceeded
-            throw new TimeError();
-          } else {
-            throw new MemoryError();
-          }
-        });
-
-        console.log(`stdout: ${output.stdout}`);
-        return output.stdout;
+        cmd = `firejail --overlay-tmpfs --quiet --noprofile --net=none --rlimit-as=134217728 --timeout=00:00:04 ${
+          file.path
+        }`;
+        queue = execCppQueue;
+        break;
       }
       default:
         return new InvalidTypeError();
+    }
+
+    for (subtask of subtasks) {
+      for (dataset of subtask["dataset"]) {
+        console.log(`Handling input ${dataset["input"]}`);
+        // Create job
+        const job = queue.createJob({ cmd, input: dataset["input"] });
+
+        // On job successful, emit socket
+        job
+
+          .on("succeeded", function(result) {
+            console.log("completed job " + job.id + " result ", result);
+            io.to(user).emit("alert", {
+              type: "result",
+              job_id: job.id,
+              status: "Completed",
+              result
+            });
+          })
+
+          // On job failure, emit socket
+          .on("failed", err => {
+            console.log(`job failed with error ${err.message}.`);
+            io.to(user).emit("alert", {
+              type: "result",
+              job_id: job.id,
+              status: "Completed",
+              error: err.message
+            });
+          })
+
+          .save();
+      }
     }
   });
 });

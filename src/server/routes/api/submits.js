3c06dc16dcfbfc82f67864efe9584c0421e17f32
mcreng <tsehonam@gmail.com>
2018-12-29 14:00:53 +0800

    feat: submission max score query api

--- src/server/routes/api/submits.js
+++ src/server/routes/api/submits.js
@@ -61,6 +61,59 @@ const queryExecutions = submission_id => {
 };
 
 /**
+ * Query the maximum scores achieved of each question by user_id.
+ *
+ * @param {String} user_id
+ * @returns {Promise} Model.aggregate promise
+ */
+const queryMaxScores = user_id => {
+  if (user_id === undefined)
+    return Submission.aggregate([
+      {
+        $group: {
+          _id: {
+            user_id: "$user_id",
+            question_id: "$question_id",
+            score: "$score"
+          }
+        }
+      },
+      {
+        $group: {
+          _id: { user_id: "$_id.user_id", question_id: "$_id.question_id" },
+          score: { $max: "$_id.score" }
+        }
+      },
+      {
+        $project: {
+          _id: "$_id.user_id",
+          question_id: "$_id.question_id",
+          score: "$score"
+        }
+      }
+    ])
+      .exec()
+      .then(doc => {
+        return doc;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  else
+    return Submission.aggregate([
+      { $match: { user_id } },
+      { $group: { _id: "$question_id", score: { $max: "$score" } } }
+    ])
+      .exec()
+      .then(doc => {
+        return doc;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+};
+
+/**
  * query parameters:
  * userid: submits of which user to display, if not sepecified it is logged in user, not logged in error
  * page: for infinite loading, each page contains 20 submits. Not specified than page 1
